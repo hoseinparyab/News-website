fdd096165044b0b9a13676651bb093a783451967
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-02-20 19:48:58 +0330

    feat: create reusable SELECT & INSERT functions for database operations
    - Implemented generic SELECT and INSERT functions to avoid code repetition
    - Optimized database queries for better performance and maintainability
    - Ensured secure and parameterized queries to prevent SQL injection
    - Improved modularity by centralizing database operations in reusable functions

--- database/DataBase.php
+++ database/DataBase.php
@@ -1,32 +1,74 @@
 <?php
- namespace  database;
- use PDO;
- use PDOException;
 
- class  Database
- {
+namespace database;
 
-     private $connection;
-     private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE =>
-         PDO::FETCH_ASSOC, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-     private $dbHost = DB_HOST;
-     private $dbUserName = DB_USERNAME;
-     private $dbName = DB_NAME;
-     private $dbPassword = DB_PASSWORD;
+use PDO;
+use PDOException;
 
-     function __construct()
-     {
-         try
-         {
-             $this->connection = new PDO("mysql:host=" . $this->dbHost. ";dbname=" . $this->dbName,
-                 $this->dbUserName, $this->dbPassword, $this->options);
-             echo 'ok';
-         }
-         catch(PDOException $e){
-             echo $e->getMessage();
-             exit;
-         }
-     }
+class Database{
 
- }
+    private $connection;
+    private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    private $dbHost = DB_HOST;
+    private $dbUserName = DB_USERNAME;
+    private $dbName = DB_NAME;
+    private $dbPassword = DB_PASSWORD;
 
+    function __construct()
+    {
+        try
+        {
+            $this->connection = new PDO("mysql:host=" . $this->dbHost. ";dbname=" . $this->dbName, $this->dbUserName, $this->dbPassword, $this->options);
+            echo 'ok';
+        }
+        catch(PDOException $e){
+            echo $e->getMessage();
+            exit;
+        }
+    }
+
+
+    // select('select * from users');
+    // select('select * from users where id = ?', [2]);
+    public function select($sql, $values = null)
+    {
+        try{
+            $stmt = $this->connection->prepare($sql);
+            if($values == null)
+            {
+                $stmt->execute();
+            }
+            else
+            {
+                $stmt->execute($values);
+            }
+            $result = $stmt;
+            return $result;
+        }
+        catch(PDOException $e){
+            echo $e->getMessage();
+            return false;
+        }
+
+    }
+
+
+    // insert('users', ['username', 'password', 'age'], ['hosein', '1234', 30]);
+    public function insert($tableName, $fields, $values)
+    {
+        try{
+            // 'username' => 'hosein', 'password' => '1234', 'age' => 30
+            $stmt = $this->connection->prepare("INSERT INTO".$tableName."(".implode(', ', $fields).
+                " , created_at) VALUES ( :" . implode(', :', $fields) . " , now() );");
+            $stmt->execute(array_combine($fields, $values));
+            return true;
+        }
+        catch(PDOException $e){
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+
+
+}
