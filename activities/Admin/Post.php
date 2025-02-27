d541165f0e15dccd07bb0b1b158f7e93e56fb3e8
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-02-27 23:41:15 +0330

    feat: add news editing functionality
    - Implemented the ability to edit existing news posts
    - Integrated CKEditor for updating news content with rich text formatting
    - Added validation to ensure proper content updates
    - Optimized backend logic for efficient news post modifications
    - Enhanced UI/UX for seamless news editing in the admin panel

--- activities/Admin/Post.php
+++ activities/Admin/Post.php
@@ -23,8 +23,8 @@ class Post extends Admin{
 
     public function store($request)
     {
-        $realTimestamp = substr($request['published_at'], 0, 10);
-        $request['published_at'] = date("Y-m-d H:i:s", (int)$realTimestamp);
+        $realTimestampt = substr($request['published_at'], 0, 10);
+        $request['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampt);
         $db = new DataBase();
         if($request['cat_id'] != null)
         {
@@ -47,57 +47,78 @@ class Post extends Admin{
     public function edit($id)
     {
         $db = new DataBase();
-        $category = $db->select('SELECT * FROM categories WHERE id = ?;', [$id])->fetch();
-        require_once(BASE_PATH . '/template/admin/categories/edit.php');
+        $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
+        $categories = $db->select('SELECT * FROM categories ORDER BY `id` DESC');
+        require_once(BASE_PATH . '/template/admin/posts/edit.php');
     }
 
     public function update($request, $id)
     {
+        $realTimestampt = substr($request['published_at'], 0, 10);
+        $request['published_at'] = date("Y-m-d H:i:s", (int)$realTimestampt);
         $db = new DataBase();
-        $db->update('categories', $id, array_keys($request), $request);
-        $this->redirect('admin/category');
+        if($request['cat_id'] != null)
+        {
+            if($request['image']['tmp_name'] != null)
+            {
+                $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
+                $this->removeImage($post['image']);
+                $request['image'] = $this->saveImage($request['image'], 'post-image');
+            }
+
+            else{
+                unset($request['image']);
+            }
+            $request = array_merge($request, ['user_id' => 1]);
+            $db->update('posts', $id , array_keys($request), $request);
+            $this->redirect('admin/post');
+        }
+        else{
+            $this->redirect('admin/post');
+        }
+
 
     }
 
     public function delete($id)
     {
+
         $db = new DataBase();
         $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
         $this->removeImage($post['image']);
         $db->delete('posts', $id);
         $this->redirectBack();
     }
+
     public function selected($id)
     {
+
         $db = new DataBase();
         $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
-        if (empty($post))
-        {
+        if(empty($post)){
             $this->redirectBack();
-
         }
-        if ($post['selected']==1){
-            $db->update('posts', $id,['selected'],[2]);
-        }else{
-            $db->update('posts', $id,['selected'],[1]);
-
+        if($post['selected'] == 1){
+            $db->update('posts', $id, ['selected'], [2]);
+        }
+        else{
+            $db->update('posts', $id, ['selected'], [1]);
         }
         $this->redirectBack();
     }
+
     public function breakingNews($id)
     {
         $db = new DataBase();
         $post = $db->select('SELECT * FROM posts WHERE id = ?;', [$id])->fetch();
-        if (empty($post))
-        {
+        if(empty($post)){
             $this->redirectBack();
-
         }
-        if ($post['breaking_news']==1){
-            $db->update('posts', $id,['breaking_news'],[2]);
-        }else{
-            $db->update('posts', $id,['breaking_news'],[1]);
-
+        if($post['breaking_news'] == 1){
+            $db->update('posts', $id, ['breaking_news'], [2]);
+        }
+        else{
+            $db->update('posts', $id, ['breaking_news'], [1]);
         }
         $this->redirectBack();
     }
