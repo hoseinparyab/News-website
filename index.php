fcd9bafddc6fa0fe35b844979e1690bf50652f4c
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-02-27 15:19:38 +0330

    feat: implement posts section in admin panel
    - Added functionality to create, edit, update, and delete posts
    - Designed and structured the admin panel interface for managing posts
    - Integrated validation and error handling for post management
    - Optimized database queries for efficient post handling
    - Improved UI/UX for a seamless content management experience

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ require_once 'database/DataBase.php';
 require_once 'database/CreateDB.php';
 require_once 'activities/Admin/Admin.php';
 require_once 'activities/Admin/Category.php';
+require_once 'activities/Admin/Post.php';
 // $db = new database\Database();
 // $db = new database\CreateDB();
 // $db->run();
@@ -178,6 +179,15 @@ uri('admin/category/store', 'Admin\Category', 'store', 'POST');
 uri('admin/category/edit/{id}', 'Admin\Category', 'edit');
 uri('admin/category/update/{id}', 'Admin\Category', 'update', 'POST');
 uri('admin/category/destroy/{id}', 'Admin\Category', 'destroy');
+
+// post
+
+uri('admin/post', 'Admin\Post', 'index');
+uri('admin/post/create', 'Admin\Post', 'create');
+uri('admin/post/store', 'Admin\Post', 'store', 'POST');
+uri('admin/post/edit/{id}', 'Admin\Post', 'edit');
+uri('admin/post/update/{id}', 'Admin\Post', 'update', 'POST');
+uri('admin/post/destroy/{id}', 'Admin\Post', 'destroy');
 
 
 echo '404 - page not found';
--- template/admin/categories/index.php
+++ template/admin/categories/index.php
@@ -7,7 +7,8 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h5"><i class="fas fa-newspaper"></i> Categories</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <a role="button" href="<?= url('admin/category/create') ?>" class="btn btn-sm btn-success">create
+            <a role="button" href="<?= url('admin/category/create') ?>" class="btn btn-sm btn-success">
+                create
             </a>
         </div>
     </div>
@@ -31,16 +32,8 @@ require_once(BASE_PATH . '/template/admin/layouts/header.php');
                         <?= $category['name'] ?>
                     </td>
                     <td>
-                        <a role="button" href="<?= url('admin/category/edit/' . $category['id']) ?>" class="btn btn-sm
-                        btn-info my-0
-                        mx-1
-                        text-white">update</a>
-                        <a role="button" href="<?= url('admin/category/destroy/' . $category['id']) ?>" class="btn
-                        btn-sm
-                        btn-danger
-                        my-0
-                        mx-1
-                        text-white">delete</a>
+                        <a role="button" href="<?= url('admin/category/edit/' . $category['id']) ?>" class="btn btn-sm btn-info my-0 mx-1 text-white">update</a>
+                        <a role="button" href="<?= url('admin/category/delete/' . $category['id']) ?>" class="btn btn-sm btn-danger my-0 mx-1 text-white">delete</a>
                     </td>
                 </tr>
 
